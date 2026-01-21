d19906b1d97381dd645d54fc3880ab6f1d28f2b0
Aditya21-dev <adityadas0217@gmail.com>
2026-01-21 22:35:28 +0530

    start bulding form validation

--- KFC_project/KFC_project/KFC_app/models.py
+++ KFC_project/KFC_project/KFC_app/models.py
@@ -5,4 +5,5 @@ from django.db import models
 class KFC_User(models.Model):
     user_name = models.CharField(max_length=50)
     email = models.EmailField()
+    contact = models.CharField(max_length=10)
     password = models.CharField(max_length=15)
