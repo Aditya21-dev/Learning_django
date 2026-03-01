3a9a0cc8d6acdebb3bb730105d993df7e403e7e3
Aditya21-dev <adityadas0217@gmail.com>
2026-03-01 23:49:44 +0530

    Model relation one to one

--- ModelRelation_practice/project/app/views.py
+++ ModelRelation_practice/project/app/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Teacher , Department
 
 # Create your views here.
+
+def landing(req):
+    return render(req,"landing.html")
+
+def forward_access(req,id):
+    Teachers = Teacher.objects.get(id = id)
+
+    departments = Teachers.department  #Forword Access 
+
+    data = {
+        "teacher_name": Teachers.name,
+        "subject": Teachers.subject,
+        "department_name": departments.name,
+        "department_building": departments.building,
+        "hod": departments.hod_name
+    }
+    return render(req,"result.html",{"data":data})
+
+
+def result(req):
+    return render(req,"result.html")
+
+
+def backword_access(req,id):
+    department = Department.objects.get(id=id)
+
+    teachers = department.teachers  # Reverse Access
+
+    context = {
+        "department": department,
+        "teachers": teachers
+    }
+
+    return render(req, "result.html", {"context":context})
