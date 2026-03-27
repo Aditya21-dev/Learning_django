fad6bec3091592124c93b23bc4a72efd4c927f62
Aditya21-dev <adityadas0217@gmail.com>
2026-03-27 23:54:44 +0530

    make small things

--- Django_Project2/project/app/models.py
+++ Django_Project2/project/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Dishes(models.Model):
+    name = models.CharField()
+    image = models.ImageField() 
+    price = models.CharField()
+    type = models.BooleanField()
--- Django_Project2/project/app/urls.py
+++ Django_Project2/project/app/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('cart/', views.cart, name='cart'),
 
 
-
     path('Owner_dashboard/', views.admin, name='Admin_dashboard'),
+    path('Add_dishes/', views.add_dishes, name='Add_dishes'),
+    path('save_dishes/', views.save_dishes, name='save_dishes'),
+    path('show_dishes/', views.show_dishes, name='show_dishes'),
 ]
